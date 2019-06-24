9be474b4c123efe1500d312f98262aa30bdd7467
mawenlong <mwljndx@163.com>
2019-06-24 08:15:53 +0800

    fix

--- src/main/java/com/mwl/demo/service/BookService.java
+++ src/main/java/com/mwl/demo/service/BookService.java
@@ -7,13 +7,24 @@ import java.util.Set;
  * @date 2019/05/06
  */
 public interface BookService {
+    /**
+     * 添加
+     */
     boolean add(String id, String name);
-
+    /**
+     * 根据id查询
+     */
     String get(String id);
-
+    /**
+     * 根据id删除
+     */
     boolean delete(String id);
-
+    /**
+     * 修改
+     */
     boolean modify(String id, String name);
-
+    /**
+     * 获取所有名称
+     */
     Set<String> getAll();
 }
